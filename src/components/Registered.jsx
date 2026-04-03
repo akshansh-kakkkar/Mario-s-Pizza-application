2e5b64ed1b442ef1f87377883223a4834389ae4b
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-04-03 19:54:33 +0530

    added few more changes

--- src/components/Registered.jsx
+++ src/components/Registered.jsx
@@ -5,7 +5,7 @@ const Registered = () => {
   return (
     <div className="flex h-screen flex-col text-center justify-center items-center ">
       <div>
-        <img className="animate-pulse" src="/pizza-guy.png" alt="pizza guy" width={550} />
+        <img className="" src="/pizza-guy.png" alt="pizza guy" width={550} />
       </div>
         <div className=" noto2-serif font-bold text-2xl mb-7">Congratulation you have been registered successfully</div>
         <button onClick={()=> Navigate('/')} className="man3-rope bg-[#FBF3E4] drop-shadow-md border-2 hover:scale-[94%] transition-all duration-500 font-bold rounded-xl border-[#AD302F] px-3 py-2">Back to home</button>
